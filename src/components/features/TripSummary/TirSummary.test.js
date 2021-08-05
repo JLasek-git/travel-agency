6b2e6c3444e96dd4ace2c4bc16884aafa7fb7dd7
Jakub Lasek <jakublasek47@gmail.com>
2021-08-05 13:42:41 +0200

    Add test to check if image in TripSummary renders with correct params

--- src/components/features/TripSummary/TirSummary.test.js
+++ src/components/features/TripSummary/TirSummary.test.js
@@ -3,11 +3,23 @@ import {shallow} from 'enzyme';
 import TripSummary from './TripSummary';
 
 describe('Component TripSummary', () => {
-  it('should render correctly without crashing', () =>{
+  it('should render correct link, depending on prop id', () =>{
     const expectedLinkId = 'abc';
     const component = shallow(<TripSummary id={expectedLinkId} tags={['tag1, tag2, tag3']} />);
 
     const renderedLinkId = component.find('.link').prop('to');
     expect(renderedLinkId).toEqual(`/trip/${expectedLinkId}`);
+  });
+
+  it('should render correct image and image alt', () => {
+    const expectedImage = 'image.jpg';
+    const expectedImageAlt = 'name';
+
+    const component = shallow(<TripSummary image={expectedImage} name={expectedImageAlt} tags={['tag1, tag2, tag3']}/>);
+
+    expect(component.find('img').prop('src')).toEqual(expectedImage);
+    expect(component.find('img').prop('alt')).toEqual(expectedImageAlt);
+
+    console.log(component.debug());
   });
 });
